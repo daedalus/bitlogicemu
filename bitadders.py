bcd5b07b8c36d99803276c9d5b67f7fbe81946f3
Darío Clavijo <daedalus2027@gmail.com>
2016-07-16 19:44:58 -0300

    a 4bit alu WIP

--- bitadders.py
+++ bitadders.py
@@ -51,9 +51,18 @@ def mux_2bit(a,b,c,d,X0,X1):
 	Y = (a & (X1 ^ 1) & (X0 ^1)) | (b & (X0 ^1) & X1) | (c & (X1 ^1) & X0) | (d & X0 & X1)
 	return Y
 
-# this 1 bit alu supports AND,OR,ADD,SLT,SUB Ops
+# this 1 bit alu supports AND,OR,ADD,SLT,SUB,NOR Ops
 # inputs a,b,aInvert,bInvert,less,CarryInput,Op0,Op1
 # ouput CarryOut,Result,OverFlow,SET
+#
+# Func|Ctl_lns |aInv|bInv| Ops
+#-----|-----------------------------------------------
+# AND |0 0 0 0 | 0  | 0  | 00
+# OR  |0 0 0 1 | 0  | 0  | 01
+# ADD |0 0 1 0 | 0  | 0  | 10
+# SUB |0 1 1 0 | 0  | 1  | 10
+# SLT |0 1 1 1 | 0  | 1  | 11
+# NOR |1 1 0 0 | 1  | 1  | 00
 
 def alu_1bit(a,b,aInv,bInv,less,Cin,Op0,Op1):	
 	a,b = (a ^ aInv),(b ^ bInv)
@@ -63,6 +72,26 @@ def alu_1bit(a,b,aInv,bInv,less,Cin,Op0,Op1):
 	SET = (S ^ OF)
 	return (Cout,Y,OF,SET)
 
+#
+# WIP
+#
+def alu_4bit(A,B,aInv,bInv,Op0,Op1):
+
+	less = 0
+	Cout = [0,0,0,0]	
+	Y = [0,0,0,0]
+	OF = [0,0,0,0]
+	SET = [0,0,0,0]
+
+	Cout[0],Y[0],OF[0],SET[0] = alu_1bit(A[0],B[0],aInv,bInv,less,bInv,Op0,Op1)
+	Cout[1],Y[1],OF[1],SET[1] = alu_1bit(A[1],B[1],aInv,bInv,less,Cout[0],Op0,Op1)
+	Cout[2],Y[2],OF[2],SET[2] = alu_1bit(A[2],B[2],aInv,bInv,less,Cout[1],Op0,Op1)
+	Cout[3],Y[3],OF[3],SET[3] = alu_1bit(A[3],B[3],aInv,bInv,less,Cout[2],Op0,Op1)
+	
+	ZERO = ((Y[0]| Y[1] | Y[2] | Y[2])^1)
+	OF = ((OF[0]| OF[1] | OF[2] | Of[2])^1)
+	less = SET[3]
+	Cout = Cout[3]	
 
 #
 # tests zone
