3b26aef03bdf3db2aa03d30029db8933021c636c
Darío Clavijo <daedalus2027@gmail.com>
2016-07-21 20:04:47 -0300

    muxers moved to another module

--- ALU.py
+++ ALU.py
@@ -12,14 +12,7 @@
 # But there is a catch, how we can implement feedback without recursion.
 #
 import bitadders
-
-def mux_1bit(a,b,X):
-	Y = (a & (X ^ 1)) | (b & X)
-	return (Y) 
-
-def mux_2bit(a,b,c,d,X0,X1):
-	Y = (a & (X1 ^ 1) & (X0 ^1)) | (b & (X0 ^1) & X1) | (c & (X1 ^1) & X0) | (d & X0 & X1)
-	return Y
+import muxers
 
 
 # this 1 bit ALU can support AND,OR,ADD,SLT,SUB,NOR Ops
