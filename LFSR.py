885b8b333c0e56df67548247be351f1553d656c9
Darío Clavijo <clavijodario@gmail.com>
2016-08-12 18:14:47 -0300

    Left Feedback Shift Register for n-Bits

---
+++ LFSR.py
@@ -0,0 +1,30 @@
+def init_register(n):
+        register = []
+        for i in range(0,n):
+                register.append(0)
+        return register
+
+def LFSR(register,newBit):
+        new_register = init_register(len(register))
+        for i in range(0,len(register)-1):
+                new_register[i] = register[i+1]
+        new_register[i+1] = newBit
+        return new_register
+
+def test_LFSR():
+        register = init_register(8)
+        print register
+
+        for i in range(0,16):
+                register = LFSR(register,(register[0] ^ 1))
+                print register
+        
+        register = init_register(8)
+        print register
+
+        for i in range(0,16):
+                register = LFSR(register,1)
+                print register
+      
+                
+test_LFSR()
